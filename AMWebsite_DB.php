5e504218ba4ee6e19148320a9d15bf062ceb19f5
would2010123 <zenghui2012@139.com>
2016-02-18 22:29:06 +0800

    all

---
+++ AMWebsite_DB.php
@@ -0,0 +1,14 @@
+<?php 
+
+ // AMH Module - AMWebsite-2.0 (AMWebsite_DB.php)
+
+ // DB INFO
+ $AMWebsite_DBName = '23.83.231.67_4a4258e9'; 
+ $AMWebsite_DBUser = '4a4258e9_user'; 
+ $AMWebsite_DBPass = '8808eda0dd3dec4'; 
+
+ // Admin INFO
+ $AMWebsite_AdminUser = 'root'; 
+ $AMWebsite_AdminPass = 'meimmy1990425@'; 
+ $AMWebsite_AdminEmail = 'zenghu2012i@139.com'; 
+
